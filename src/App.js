915b71c444559a68f9c1436757aa9b25511574ac
Luiz Miguel <luizmiguel.oliveira18@gmail.com>
2019-03-24 15:51:06 -0300

    Setup project with custom-react-scripts

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import {  BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import { Home } from './pages'
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <Switch>
+          <Route path="/" component={ Home }/>
+        </Switch>
+      </Router>
+    );
+  }
+}
+
+export default App;
